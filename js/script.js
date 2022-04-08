92cc67327456a52043b0f387bdbcf6771d4c341e
Pavel <pavel.istomiinn@gmail.com>
2022-04-08 11:46:52 +0300

    первая часть практики

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 "use strict";
-const obj = {
-    name: "Pavel",
-    age: "22",
-    isMarried: false,
+const NumberofFFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+const personalMovieDB = {
+    count: NumberofFFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
+const a = prompt ('Один из последних просмотренных вами фильмов?', ''),
+    b = prompt ('Насколько вы его оцените?', ''),
+    c = prompt ('Один из последних просмотренных вами фильмов?', ''),
+    d = prompt ('Насколько вы его оцените?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB) ;
