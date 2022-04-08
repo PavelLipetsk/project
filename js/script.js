d8d8a90d53df4ebf5eb712e5aa919ef6564d24db
Pavel <pavel.istomiinn@gmail.com>
2022-04-08 14:44:59 +0300

    второе практическое занятие

--- js/script.js
+++ js/script.js
@@ -9,12 +9,29 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt ('Один из последних просмотренных вами фильмов?', ''),
-    b = prompt ('Насколько вы его оцените?', ''),
-    c = prompt ('Один из последних просмотренных вами фильмов?', ''),
-    d = prompt ('Насколько вы его оцените?', '');
+let i = 0;
+while (i < 2) {
+    
+    const a = prompt ('Один из последних просмотренных вами фильмов?', ''),
+          b = prompt ('Насколько вы его оцените?', '');
+    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log("error");
+        i--;
+    }
+    if (personalMovieDB.count < 10){
+        console.log("Просмотрено довльно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if ( personalMovieDB.count >= 30){
+        console.log("Вы киноман");
+    } else {
+        console.log('Ошибка');
+    }
+    i++;
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB) ;
+console.log(personalMovieDB);
